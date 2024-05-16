47520cf8bc0f1603111734a8f1713ba392653b8b
Rafael Santos <rafa.05125@gmail.com>
2024-05-16 16:16:20 -0300

    Tests

---
+++ Tests/ClienteTest.cs
@@ -0,0 +1,26 @@
+﻿using ParkingLotDapper.Models;
+
+namespace Tests
+{
+
+    [TestClass]
+    public class ClienteTest
+    {
+        [TestMethod]
+        public void TestingClienteModel()
+        {
+            // Arrange
+            var cliente = new Cliente();
+
+            //  Act
+            cliente.Id = 1;
+            cliente.Nome = "Danilo";
+            cliente.Cpf = "653.027.290-91";
+
+            // Assert
+            Assert.AreEqual(1, cliente.Id);
+            Assert.AreEqual("Danilo", cliente.Nome);
+            Assert.AreEqual("653.027.290-91", cliente.Cpf);
+        }
+    }
+}
---
+++ Tests/TicketTest.cs
@@ -0,0 +1,39 @@
+﻿using ParkingLotDapper.Models;
+
+[TestClass]
+public class TicketTest
+{
+    [TestMethod]
+    public void TestingTotalValueMethod()
+    {
+        // Arrange 
+        var cliente = new Cliente { Id = 1, Nome = "Danilo" };
+        var veiculo = new Veiculo { Id = 1, Marca = "Fiat", Modelo = "Uno", ClienteId = cliente.Id, Placa = "DASS134" };
+        var ticket = new Ticket { DataEntrada = DateTime.Now.AddHours(-1), Id = 1, VagaId = 1, VeiculoId = veiculo.Id };
+        var valorDoMinuto = new ValorDoMinuto { Minutos = 1, Valor = 1 };
+        var totalDesejadoDeMinuto = 60.0;
+
+        // Act 
+        var valorTotal = ticket.TotalValue(valorDoMinuto);
+
+        // Assert 
+        Assert.AreEqual(totalDesejadoDeMinuto, valorTotal);
+    }
+
+    public void TestingTicketPaidValue()
+    {
+        // Arrange 
+        var cliente = new Cliente { Id = 1, Nome = "Danilo" };
+        var veiculo = new Veiculo { Id = 1, Marca = "Fiat", Modelo = "Uno", ClienteId = cliente.Id, Placa = "DASS134" };
+        var ticket = new Ticket { DataEntrada = DateTime.Now.AddHours(-1), Id = 1, VagaId = 1, VeiculoId = veiculo.Id };
+        var valorDoMinuto = new ValorDoMinuto { Minutos = 1, Valor = 1 };
+        var valorTotalDesejado = ticket.TotalValue(valorDoMinuto);
+
+        // Act 
+        ticket.Pago(valorDoMinuto);
+
+        // Assert 
+        Assert.AreEqual(valorTotalDesejado, ticket.Valor);
+        Assert.IsNotNull(ticket.DataSaida);
+    }
+}
